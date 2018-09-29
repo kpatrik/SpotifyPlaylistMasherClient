dc3ed88fcd6744598e51fedfe56bbb6e86512510
Patrik Koleszár <patrik5000@gmail.com>
2018-09-29 12:23:59 +0200

    feat(App): communicate with server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      response: ""
+    }
+  }
+
+  componentWillMount() {
+    axios.get('http://localhost:3001', { headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    }})
+      .then((response) => {
+        console.log(response);
+        this.setState({response: response.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Sportify Playlist Masher</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The response from the server: {this.state.response}
         </p>
       </div>
     );
